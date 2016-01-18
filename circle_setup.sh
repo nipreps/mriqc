485fe9d68067251d8939f92f40ace39e959e547c
Oscar Esteban <code@oscaresteban.es>
2016-01-18 01:46:53 +0100

    force download of md5, use -c in wget

--- circle_setup.sh
+++ circle_setup.sh
@@ -7,13 +7,8 @@ set -e
 mkdir -p ${HOME}/downloads
 
 # Download md5 checksums
-if [[ ! -s ${HOME}/downloads/fsl-5.0.9-centos6_64.tar.gz.md5 ]]; then
-    wget -P ${HOME}/downloads/ "http://fsl.fmrib.ox.ac.uk/fsldownloads/md5sums/fsl-5.0.9-centos6_64.tar.gz.md5"
-fi
-
-if [[ ! -s ${HOME}/downloads/linux_openmp_64.tgz.md5 ]]; then
-    wget -O ${HOME}/downloads/linux_openmp_64.tgz.md5 "https://drive.google.com/open?id=0BxI12kyv2olZV0tpZ3VJYjJ6NWM"
-fi
+wget -P ${HOME}/downloads/ "http://fsl.fmrib.ox.ac.uk/fsldownloads/md5sums/fsl-5.0.9-centos6_64.tar.gz.md5"
+wget -O ${HOME}/downloads/linux_openmp_64.tgz.md5 "https://docs.google.com/uc?authuser=1&id=0BxI12kyv2olZV0tpZ3VJYjJ6NWM&export=download"
 
 cd ${HOME}/downloads
 fslchk=$(md5sum -c fsl-5.0.9-centos6_64.tar.gz.md5 | awk '{print $2}')
@@ -22,7 +17,7 @@ cd
 
 # Get fsl if md5 is not ok
 if [ $fslchk != "OK" ]; then 
-    wget -P ${HOME}/downloads/ "http://fsl.fmrib.ox.ac.uk/fsldownloads/fsl-5.0.9-centos6_64.tar.gz"
+    wget -P ${HOME}/downloads/ -c "http://fsl.fmrib.ox.ac.uk/fsldownloads/fsl-5.0.9-centos6_64.tar.gz"
 fi
 
 if [[ ! -d ${HOME}/fsl ]]; then 
@@ -32,7 +27,7 @@ source ${HOME}/fsl/etc/fslconf/fsl.sh
 
 # Get afni if md5 is not ok
 if [ $afnichk != "OK" ]; then
-    wget -P ${HOME}/downloads/ "http://afni.nimh.nih.gov/pub/dist/tgz/linux_openmp_64.tgz"
+    wget -P ${HOME}/downloads/ -c "http://afni.nimh.nih.gov/pub/dist/tgz/linux_openmp_64.tgz"
 fi
 
 if [[ ! -d ${HOME}/afni ]]; then
