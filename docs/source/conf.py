84e673cc2508c3025ca672302f251e312495b4dd
Oscar Esteban <code@oscaresteban.es>
2016-03-11 17:09:14 -0800

    fix for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,13 @@
 import sys
 import os
 
+# Hack for readthedocs
+# http://blog.rtwilson.com/how-to-make-your-sphinx-documentation-compile-with-readthedocs-when-youre-using-numpy-and-scipy/
+import mock
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
