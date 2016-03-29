c10732f5069c7c17ea9216cc27b96582e68a5457
oesteban <code@oscaresteban.es>
2016-03-29 14:55:31 -0700

    add caching to new docker circleci

--- circle_setup.sh
+++ circle_setup.sh
@@ -4,14 +4,14 @@ set -x
 set -e
 
 # Get test data
-if [[ ! -d /scratch/data/ds003_downsampled ]]; then
+if [[ ! -d ${HOME}/scratch/data/ds003_downsampled ]]; then
     # Folder for downloads
     mkdir -p ${HOME}/downloads
     wget -P ${HOME}/downloads/ "https://googledrive.com/host/0B2JWN60ZLkgkMEw4bW5VUUpSdFU/ds003_downsampled.tar"
-    mkdir -p /scratch/data/
-    tar xf ${HOME}/downloads/ds003_downsampled.tar -C /scratch/data
+    mkdir -p ${HOME}/scratch/data/
+    tar xf ${HOME}/downloads/ds003_downsampled.tar -C ${HOME}/scratch/data
 fi
 
-mkdir -p /scratch/data/ds003_sub-01
-ln -fs /scratch/data/ds003_downsampled/sub-01 /scratch/data/ds003_sub-01/
-echo "{plugin: MultiProc, plugin_args: {n_proc: 2}}" > /scratch/data/plugin.yml
+mkdir -p ${HOME}/scratch/data/ds003_sub-01
+ln -fs ${HOME}/scratch/data/ds003_downsampled/sub-01 ${HOME}/scratch/data/ds003_sub-01/
+echo "{plugin: MultiProc, plugin_args: {n_proc: 2}}" > ${HOME}/scratch/plugin.yml
