e75c2de05df58b3db280ec878f4bccf79c55916e
Oscar Esteban <code@oscaresteban.es>
2016-03-31 19:00:04 -0700

    fix circle_tests

---
+++ build/circle_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+set -e
+
+declare -a MRIQC_TESTS=(
+	"-i data/ds003_downsampled -o outputs/ms-func/out -w outputs/ms-func/work --skip-anatomical" #
+	"-i data/ds003_downsampled -o outputs/ms-anat/out -w outputs/ms-anat/work --skip-functional --use-plugin plugin.yml --save-memory" #
+# -i data/ds003_sub-01/ -o outputs/ss-all/out -w outputs/ss-all/work
+)
+
+i=0
+for test_params in "${MRIQC_TESTS[@]}"; do
+	if [ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]; then
+	    docker run -i -e TZ=PST -v ~/scratch:/scratch -w /scratch oesteban/mriqc ${test_params}
+	fi
+	((i=i+1))
+done
