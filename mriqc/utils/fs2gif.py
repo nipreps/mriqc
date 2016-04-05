92559de19ea77deebca18ecfdde792f16ff76af2
oesteban <code@oscaresteban.es>
2016-04-05 10:42:08 -0700

    added some improvements #65

--- mriqc/utils/fs2gif.py
+++ mriqc/utils/fs2gif.py
@@ -21,6 +21,7 @@ import glob
 from six import string_types
 import numpy as np
 import nibabel as nb
+from skimage import exposure
 
 def main():
     """Entry point"""
@@ -31,6 +32,8 @@ def main():
     g_input.add_argument('-s', '--subject-id', action='store')
     g_input.add_argument('-t', '--temp-dir', action='store')
     g_input.add_argument('--keep-temp', action='store_true', default=False)
+    g_input.add_argument('--zoom', action='store_true', default=False)
+    g_input.add_argument('--hist-eq', action='store_true', default=False)
     g_outputs = parser.add_argument_group('Outputs')
     g_outputs.add_argument('-o', '--output-dir', action='store', default='fs2gif')
 
@@ -71,7 +74,6 @@ def main():
     environ['SUBJECTS_DIR'] = subjects_dir
     # tcl_file = pkgr.resource_filename('mriqc', 'data/fsexport.tcl')
     tcl_contents = """
-SetZoomLevel 2
 SetOrientation 0
 SetCursor 0 128 128 128
 SetDisplayFlag 3 0
@@ -81,9 +83,17 @@ set i 0
 
     for sub_path in subject_list:
         subid = op.basename(sub_path)
-        niifile = op.join(tmpdir, '%s.nii.gz') % subid
+        tmp_sub = op.join(tmpdir, subid)
+        try:
+            os.makedirs(tmp_sub)
+        except OSError as exc:
+            if exc.errno != EEXIST:
+                raise exc
+
+        niifile = op.join(tmp_sub, '%s.nii.gz') % subid
+        ref_file = op.join(sub_path, 'mri', 'T1.mgz')
         sp.call(['mri_convert', op.join(sub_path, 'mri', 'norm.mgz'), niifile],
-                cwd=tmpdir)
+                cwd=tmp_sub)
         data = nb.load(niifile).get_data()
         data[data > 0] = 1
 
@@ -93,46 +103,75 @@ set i 0
         bbox_max = indexes.max(0) + 1
         center = np.average([bbox_min, bbox_max], axis=0)
 
-        # Export tiffs for left hemisphere
-        tcl_file = op.join(tmpdir, 'lh-%s.tcl' % subid)
-        with open(tcl_file, 'w') as tclfp:
-            tclfp.write(tcl_contents)
-            tclfp.write('for { set slice %d } { $slice < %d } { incr slice } {' % (bbox_min[2], bbox_max[2]))
-            tclfp.write('    SetZoomCenter %d %d $slice\n' % (center[0] + 30, center[1] - 10))
-            tclfp.write('    SetSlice $slice\n')
-            tclfp.write('    RedrawScreen\n')
-            tclfp.write('    SaveTIFF [format "%s/%s-lh-' % (tmpdir, subid) + '%03d.tif" $i]\n')
-            tclfp.write('    incr i\n')
-            tclfp.write('}\n')
-            tclfp.write('QuitMedit\n')
-        sp.call(['tkmedit', subid, 'norm.mgz', 'lh.white', '-tcl', tcl_file], env=environ)
-
-        # Export tiffs for right hemisphere
-        tcl_file = op.join(tmpdir, 'rh-%s.tcl' % subid)
-        with open(tcl_file, 'w') as tclfp:
-            tclfp.write(tcl_contents)
-            tclfp.write('for { set slice %d } { $slice < %d } { incr slice } {' % (bbox_min[2], bbox_max[2]))
-            tclfp.write('    SetZoomCenter %d %d $slice\n' % (center[0] - 30, center[1] - 10))
-            tclfp.write('    SetSlice $slice\n')
-            tclfp.write('    RedrawScreen\n')
-            tclfp.write('    SaveTIFF [format "%s/%s-rh-' % (tmpdir, subid) + '%03d.tif" $slice]\n')
-            tclfp.write('    incr i\n')
-            tclfp.write('}\n')
-            tclfp.write('QuitMedit\n')
-        sp.call(['tkmedit', subid, 'norm.mgz', 'rh.white', '-tcl', tcl_file], env=environ)
-
-        # Convert to animated gif
-        sp.call(['convert', '-delay', '10', '-loop', '0', '%s/%s-lh-*.tif' % (tmpdir, subid),
-                 '%s/%s-lh.gif' % (out_dir, subid)])
-        sp.call(['convert', '-delay', '10', '-loop', '0', '%s/%s-rh-*.tif' % (tmpdir, subid),
-                 '%s/%s-rh.gif' % (out_dir, subid)])
+        if opts.hist_eq:
+            modnii = op.join(tmp_sub, '%s.nii.gz' % subid)
+            ref_file = op.join(tmp_sub, '%s.mgz' % subid)
+            img = nb.load(niifile)
+            data = exposure.equalize_adapthist(img.get_data(), clip_limit=0.03)
+            nb.Nifti1Image(data, img.get_affine(), img.get_header()).to_filename(modnii)
+            sp.call(['mri_convert', modnii, ref_file], cwd=tmp_sub)
 
 
-    if not opts.keep_temp:
-        try:
-            rmtree(tmpdir)
-        except:
-            pass
+        if not opts.zoom:
+            # Export tiffs for left hemisphere
+            tcl_file = op.join(tmp_sub, '%s.tcl' % subid)
+            with open(tcl_file, 'w') as tclfp:
+                tclfp.write(tcl_contents)
+                tclfp.write('for { set slice %d } { $slice < %d } { incr slice } {' % (bbox_min[2], bbox_max[2]))
+                tclfp.write('    SetSlice $slice\n')
+                tclfp.write('    RedrawScreen\n')
+                tclfp.write('    SaveTIFF [format "%s/%s-' % (tmp_sub, subid) + '%03d.tif" $i]\n')
+                tclfp.write('    incr i\n')
+                tclfp.write('}\n')
+                tclfp.write('QuitMedit\n')
+            sp.call(['tkmedit', subid, 'T1.mgz', 'lh.pial', '-aux-surface', 'rh.pial', '-tcl', tcl_file], env=environ)
+            # Convert to animated gif
+            sp.call(['convert', '-delay', '10', '-loop', '0', '%s/%s-*.tif' % (tmp_sub, subid),
+                     '%s/%s.gif' % (out_dir, subid)])
+
+        else:
+            # Export tiffs for left hemisphere
+            tcl_file = op.join(tmp_sub, 'lh-%s.tcl' % subid)
+            with open(tcl_file, 'w') as tclfp:
+                tclfp.write(tcl_contents)
+                tclfp.write('SetZoomLevel 2')
+                tclfp.write('for { set slice %d } { $slice < %d } { incr slice } {' % (bbox_min[2], bbox_max[2]))
+                tclfp.write('    SetZoomCenter %d %d $slice\n' % (center[0] + 30, center[1] - 10))
+                tclfp.write('    SetSlice $slice\n')
+                tclfp.write('    RedrawScreen\n')
+                tclfp.write('    SaveTIFF [format "%s/%s-lh-' % (tmp_sub, subid) + '%03d.tif" $i]\n')
+                tclfp.write('    incr i\n')
+                tclfp.write('}\n')
+                tclfp.write('QuitMedit\n')
+            sp.call(['tkmedit', subid, 'norm.mgz', 'lh.white', '-tcl', tcl_file], env=environ)
+    
+            # Export tiffs for right hemisphere
+            tcl_file = op.join(tmp_sub, 'rh-%s.tcl' % subid)
+            with open(tcl_file, 'w') as tclfp:
+                tclfp.write(tcl_contents)
+                tclfp.write('SetZoomLevel 2')
+                tclfp.write('for { set slice %d } { $slice < %d } { incr slice } {' % (bbox_min[2], bbox_max[2]))
+                tclfp.write('    SetZoomCenter %d %d $slice\n' % (center[0] - 30, center[1] - 10))
+                tclfp.write('    SetSlice $slice\n')
+                tclfp.write('    RedrawScreen\n')
+                tclfp.write('    SaveTIFF [format "%s/%s-rh-' % (tmp_sub, subid) + '%03d.tif" $slice]\n')
+                tclfp.write('    incr i\n')
+                tclfp.write('}\n')
+                tclfp.write('QuitMedit\n')
+            sp.call(['tkmedit', subid, 'norm.mgz', 'rh.white', '-tcl', tcl_file], env=environ)
+    
+            # Convert to animated gif
+            sp.call(['convert', '-delay', '10', '-loop', '0', '%s/%s-lh-*.tif' % (tmp_sub, subid),
+                     '%s/%s-lh.gif' % (out_dir, subid)])
+            sp.call(['convert', '-delay', '10', '-loop', '0', '%s/%s-rh-*.tif' % (tmp_sub, subid),
+                     '%s/%s-rh.gif' % (out_dir, subid)])
+
+
+        if not opts.keep_temp:
+            try:
+                rmtree(tmp_sub)
+            except:
+                pass
 
 
 if __name__ == '__main__':
