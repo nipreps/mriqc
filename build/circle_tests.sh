f3a719ff24413d42113075a17f20be37d2a45b06
oesteban <code@oscaresteban.es>
2016-04-15 10:45:55 -0700

    fix typo previous commit

--- build/circle_tests.sh
+++ build/circle_tests.sh
@@ -10,13 +10,13 @@ declare -a MRIQC_TESTS=(
 )
 
 if [[ $CIRCLE_NODE_INDEX -eq 0 ]]; then
-	docker run -i -v /etc/localtime:/etc/localtime:ro-e TZ=PST --entrypoint="/usr/bin/run_tests" oesteban/mriqc
+	docker run -i -v /etc/localtime:/etc/localtime:ro --entrypoint="/usr/bin/run_tests" oesteban/mriqc
 fi
 
 i=0
 for test_params in "${MRIQC_TESTS[@]}"; do
 	if [ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]; then
-	    docker run -i -e TZ=PST -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch oesteban/mriqc ${test_params}
+	    docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch oesteban/mriqc ${test_params}
 	fi
 	((i=i+1))
 done
