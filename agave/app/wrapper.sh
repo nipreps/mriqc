78754ea44448f23b16b59d286b9df0496fc7b608
Oscar Esteban <code@oscaresteban.es>
2016-04-15 22:29:02 -0700

    remove old app, add options in agave app generator

--- agave/app/wrapper.sh
+++ agave/app/wrapper.sh
@@ -4,16 +4,18 @@
 # @Last Modified by:   oesteban
 # @Last Modified time: 2015-11-13 10:00:55
 
-
+OUTPUT_FOLDER=$SCRATCH/${AGAVE_JOB_NAME}-${AGAVE_JOB_ID}
 # Create output folder in scratch
-mkdir -p $SCRATCH/mriqcp
+mkdir -p ${OUTPUT_FOLDER}
+
+echo "Running mriqc on ${bidsFile}" >> ${OUTPUT_FOLDER}/${AGAVE_JOB_NAME}.out
 
 # Activate anaconda env
-source activate qap
+source activate crn-2.7
 
 # Call mriqcp
-FSLOUTPUTTYPE='NIFTI_GZ' mriqc -i ds003_downsampled -o $SCRATCH/mriqcp/out -w $SCRATCH/mriqcp/tmp
+FSLOUTPUTTYPE='NIFTI_GZ' mriqc -i ${bidsFile} -o ${OUTPUT_FOLDER}/out -w ${OUTPUT_FOLDER}/tmp  2>> ${OUTPUT_FOLDER}/${AGAVE_JOB_NAME}.err 1>> ${OUTPUT_FOLDER}/${AGAVE_JOB_NAME}.out
 
 # Collect outputs
-archivepath=$(echo $SCRATCH/mriqcp/out | cut -d: -f2 | xargs)
+archivepath=$(echo ${OUTPUT_FOLDER}/out | cut -d: -f2 | xargs)
 ln -s $archivepath .
