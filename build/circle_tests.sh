8e33484185fa0b38726f65d8bc493c2048e12b8d
Oscar Esteban <code@oscaresteban.es>
2016-04-16 21:13:03 -0700

    fix path to csv outputs

--- build/circle_tests.sh
+++ build/circle_tests.sh
@@ -22,7 +22,7 @@ for test_params in "${MRIQC_TESTS[@]}"; do
 done
 
 if [[ $CIRCLE_NODE_INDEX -eq 0 ]]; then
-	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch --entrypoint="/usr/bin/run_dfcheck" oesteban/mriqc -i /scratch/out/fMRIQC.csv -r /root/src/mriqc/mriqc/data/tests/ds003_downsampled_fMRI.csv
+	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch --entrypoint="/usr/bin/run_dfcheck" oesteban/mriqc -i /scratch/outputs/ms-func/out/fMRIQC.csv -r /root/src/mriqc/mriqc/data/tests/ds003_downsampled_fMRI.csv
 elif [[ $CIRCLE_NODE_INDEX -eq 1 ]]; then
-	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch --entrypoint="/usr/bin/run_dfcheck" oesteban/mriqc -i /scratch/out/aMRIQC.csv -r /root/src/mriqc/mriqc/data/tests/ds003_downsampled_sMRI.csv
+	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch --entrypoint="/usr/bin/run_dfcheck" oesteban/mriqc -i /scratch/outputs/ms-anat/out/aMRIQC.csv -r /root/src/mriqc/mriqc/data/tests/ds003_downsampled_sMRI.csv
 fi
