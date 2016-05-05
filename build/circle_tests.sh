8c26902273db85fdaaf9f17279d82347492d7ffd
oesteban <code@oscaresteban.es>
2016-05-05 12:01:28 -0700

    remove dfcheck data, add functional back to tests-

--- build/circle_tests.sh
+++ build/circle_tests.sh
@@ -19,13 +19,14 @@ set -e
 # done
 
 if [[ $CIRCLE_NODE_INDEX -eq 0 ]]; then
-	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch oesteban/mriqc -B /scratch/data/ds003_downsampled -S $(seq -f '%02g' 01 06) -d anat -o outputs/ms-anat/out -w outputs/ms-anat/work
-	docker run -i -v /etc/localtime:/etc/localtime:ro --entrypoint="/usr/bin/run_tests" oesteban/mriqc
-#	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch --entrypoint="/usr/bin/run_dfcheck" oesteban/mriqc -i /scratch/outputs/ms-func/out/funcMRIQC.csv -r /root/src/mriqc/mriqc/data/tests/ds003_downsampled_fMRI.csv
+	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch oesteban/mriqc -B /scratch/data/ds003_downsampled -d func -o outputs/ms-func/out -w outputs/ms-func/work
+	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch --entrypoint="/usr/bin/run_mriqc_plot" oesteban/mriqc -d func -o outputs/ms-func/out -w outputs/ms-func/work
+	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch oesteban/mriqc -B /scratch/data/ds003_downsampled -S $(seq -f '%02g' 01 05) -d anat -o outputs/ms-anat/out -w outputs/ms-anat/work
+	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch --entrypoint="/usr/bin/run_mriqc_plot" oesteban/mriqc -d anat -o outputs/ms-anat/out -w outputs/ms-anat/work
 fi
 
 if [[ $CIRCLE_NODE_INDEX -eq 1 ]]; then
-	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch oesteban/mriqc -B /scratch/data/ds003_downsampled -S $(seq -f '%02g' 07 13) -d anat -o outputs/ms-anat/out -w outputs/ms-anat/work
+	docker run -i -v /etc/localtime:/etc/localtime:ro --entrypoint="/usr/bin/run_tests" oesteban/mriqc
+	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch oesteban/mriqc -B /scratch/data/ds003_downsampled -S $(seq -f '%02g' 06 13) -d anat -o outputs/ms-anat/out -w outputs/ms-anat/work
 	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch --entrypoint="/usr/bin/run_mriqc_plot" oesteban/mriqc -d anat -o outputs/ms-anat/out -w outputs/ms-anat/work
-	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch --entrypoint="/usr/bin/run_dfcheck" oesteban/mriqc -i /scratch/outputs/ms-anat/out/anatMRIQC.csv -r /root/src/mriqc/mriqc/data/tests/ds003_downsampled_sMRI.csv
 fi
