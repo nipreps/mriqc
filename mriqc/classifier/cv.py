28ea38a613a3875410a2671a90c793d69b4ab07d
oesteban <code@oscaresteban.es>
2016-05-17 13:58:02 -0700

    add permutation tests

--- mriqc/classifier/cv.py
+++ mriqc/classifier/cv.py
@@ -3,7 +3,7 @@
 # @Author: oesteban
 # @Date:   2015-11-19 16:44:27
 # @Last Modified by:   oesteban
-# @Last Modified time: 2016-05-13 11:49:25
+# @Last Modified time: 2016-05-17 13:57:47
 
 """
 MRIQC Cross-validation
@@ -20,11 +20,14 @@ import simplejson as json
 from argparse import ArgumentParser
 from argparse import RawTextHelpFormatter
 
+import numpy as np
+from scipy.stats.mstats import zscore
 import pandas as pd
 from sklearn import svm
-from sklearn.cross_validation import LeaveOneLabelOut
+from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn.cross_validation import LeaveOneLabelOut, permutation_test_score
 from sklearn.grid_search import GridSearchCV
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, f1_score, accuracy_score
 
 def main():
     """Entry point"""
@@ -39,6 +42,17 @@ def main():
     g_input.add_argument('-N', '--site-normalization', action='store_true',
                          default=False)
     g_input.add_argument('-P', '--parameters', action='store')
+    g_input.add_argument('-C', '--classifier', action='store', nargs='*',
+                         choices=['svc_linear', 'svc_rbf', 'rfc', 'all'],
+                         default=['svc_rbf'])
+    g_input.add_argument(
+        '-S', '--score-types', action='store', nargs='*',
+        choices=['accuracy', 'adjusted_rand_score', 'average_precision', 'f1', 'f1_macro', 'f1_micro',
+                 'f1_samples', 'f1_weighted', 'log_loss', 'mean_absolute_error', 'mean_squared_error',
+                 'median_absolute_error', 'precision', 'precision_macro', 'precision_micro', 'precision_samples',
+                 'precision_weighted', 'r2', 'recall', 'recall_macro', 'recall_micro', 'recall_samples',
+                 'recall_weighted', 'roc_auc'],
+        default=['f1_weighted'])
 
     g_outputs = parser.add_argument_group('Outputs')
     g_outputs.add_argument('-o', '--out-csv', action='store',
@@ -71,48 +85,152 @@ def main():
             X_df.loc[X_df.site == site, thesecols] -= means
             X_df.loc[X_df.site == site, thesecols] -= stdvs
 
+    X_df_test = X_df.sample(n=100, random_state=31051983)
+    y_test = [int(y_i) for y_i in X_df_test.rate.values]
+    X_test = [tuple(x) for x in X_df_test[colnames].values]
+
     if opts.out_csv is not None:
         with open(opts.out_csv, 'w') as outfile:
             X_df.to_csv(outfile, index=False)
 
     lolo_folds = LeaveOneLabelOut(sites)
 
+    test_clfs = opts.classifier
+    if 'all' in opts.classifier:
+        test_clfs = ['svc_linear', 'svc_rbf', 'rfc']
+
+    if 'svc_linear' in test_clfs:
+        for stype in opts.score_types:
+            sample_x = [tuple(x) for x in X_df[colnames].values]
+            labels_y = [int(y_i) for y_i in y_df.rate.values]
+            clf = GridSearchCV(
+                svm.LinearSVC(C=1), convert([{'C': [1]}]),
+                cv=lolo_folds, scoring=stype, n_jobs=-1)
+            clf.fit(sample_x, labels_y)
+
+            print("Best parameters set found on development set:")
+            print()
+            print(clf.best_params_)
+            print()
+            print("Grid %s scores on development set:" % stype)
+            print()
+            for params, mean_score, scores in clf.grid_scores_:
+                print("%0.3f (+/-%0.03f) for %r"
+                      % (mean_score, scores.std() * 2, params))
+            print()
+            print("Detailed classification report:")
+            print()
+            print("The model is trained on the full development set.")
+            print("The scores are computed on the full evaluation set.")
+            print()
+            y_pred = clf.predict(X_test)
+            print(classification_report(y_test, y_pred))
+            print(y_pred)
+            print()
+
+            f1, acc = permutation_distribution(y_test, y_pred)
+            print("F1 z-score = %f" % zscore(f1 + [f1_score(y_test, y_pred)], ddof=1)[-1])
+            print("Acc z-score = %f" % zscore(acc + [accuracy_score(y_test, y_pred)], ddof=1)[-1])
+#            score, _, pvalue = permutation_test_score(
+#                clf, X_test, y_test, scoring="accuracy", n_permutations=100, n_jobs=1)
+#
+#            print("Classification score %s (pvalue : %s)" % (score, pvalue))
+
+    if 'svc_rbf' in test_clfs:
     # Set the parameters by cross-validation
-    tuned_parameters = convert([
-        {'kernel': ['rbf'], 'gamma': [1e-3, 1e-4], 'C': [1, 10, 100, 1000]},
-        {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}
-    ])
-    if opts.parameters is not None:
-        with open(opts.parameters, 'r') as paramfile:
-            tuned_parameters = convert(json.load(paramfile))
+        if opts.parameters is not None:
+            with open(opts.parameters, 'r') as paramfile:
+                tuned_parameters = convert(json.load(paramfile))
+        else:
+            tuned_parameters = convert([
+                {'kernel': ['rbf'], 'gamma': [1e-2, 1e-3, 1e-4], 'C': [0.1, 1, 10, 100, 1000]}
+            ])
+        for stype in opts.score_types:
+            sample_x = [tuple(x) for x in X_df[colnames].values]
+            labels_y = [int(y_i) for y_i in y_df.rate.values]
+            clf = GridSearchCV(svm.SVC(C=1), tuned_parameters, cv=lolo_folds,
+                               scoring=stype, n_jobs=-1)
+            clf.fit(sample_x, labels_y)
 
-    scores = ['precision', 'recall']
+            print("Best parameters set found on development set:")
+            print()
+            print(clf.best_params_)
+            print()
+            print("Grid %s scores on development set:" % stype)
+            print()
+            for params, mean_score, scores in clf.grid_scores_:
+                print("%0.3f (+/-%0.03f) for %r"
+                      % (mean_score, scores.std() * 2, params))
+            print()
+            print("Detailed classification report:")
+            print()
+            print("The model is trained on the full development set.")
+            print("The scores are computed on the full evaluation set.")
+            print()
+            y_pred = clf.predict(X_test)
+            print(classification_report(y_test, y_pred))
+            print()
+            f1, acc = permutation_distribution(y_test, y_pred)
+            print("F1 z-score = %f" % zscore(f1 + [f1_score(y_test, y_pred)], ddof=1)[-1])
+            print("Acc z-score = %f" % zscore(acc + [accuracy_score(y_test, y_pred)], ddof=1)[-1])
+#            score, _, pvalue = permutation_test_score(
+#                clf, X_test, y_test, scoring="accuracy", n_permutations=100, n_jobs=1)
+#
+#            print("Classification score %s (pvalue : %s)" % (score, pvalue))
 
-    for score in scores:
-        sample_x = [tuple(x) for x in X_df[colnames].values]
-        labels_y = [int(y_i) for y_i in y_df.rate.values]
-        clf = GridSearchCV(svm.SVC(C=1), tuned_parameters, cv=lolo_folds,
-                           scoring='%s_weighted' % score, n_jobs=-1)
-        clf.fit(sample_x, labels_y)
 
-        print("Best parameters set found on development set:")
-        print()
-        print(clf.best_params_)
-        print()
-        print("Grid scores on development set:")
-        print()
-        for params, mean_score, scores in clf.grid_scores_:
-            print("%0.3f (+/-%0.03f) for %r"
-                  % (mean_score, scores.std() * 2, params))
-        # print()
-        # print("Detailed classification report:")
-        # print()
-        # print("The model is trained on the full development set.")
-        # print("The scores are computed on the full evaluation set.")
-        # print()
-        # y_true, y_pred = y_test, clf.predict(X_test)
-        # print(classification_report(y_true, y_pred))
-        # print()
+    if 'rfc' in test_clfs:
+        # Set the parameters by cross-validation
+        tuned_parameters = convert([
+            {'n_estimators': range(5, 20),
+             'max_depth': [None] + range(5, 11),
+             'min_samples_split': range(1, 5)}
+        ])
+        for stype in opts.score_types:
+            sample_x = [tuple(x) for x in X_df[colnames].values]
+            labels_y = [int(y_i) for y_i in y_df.rate.values]
+            clf = GridSearchCV(RFC(), tuned_parameters, cv=lolo_folds,
+                               scoring=stype, n_jobs=-1)
+            clf.fit(sample_x, labels_y)
+
+            print("Best parameters set found on development set:")
+            print()
+            print(clf.best_params_)
+            print()
+            print("Grid %s scores on development set:" % stype)
+            print()
+            for params, mean_score, scores in clf.grid_scores_:
+                print("%0.3f (+/-%0.03f) for %r"
+                      % (mean_score, scores.std() * 2, params))
+            print()
+            print("Detailed classification report:")
+            print()
+            print("The model is trained on the full development set.")
+            print("The scores are computed on the full evaluation set.")
+            print()
+            y_pred = clf.predict(X_test)
+            print(classification_report(y_test, y_pred))
+            print()
+            f1, acc = permutation_distribution(y_test, y_pred)
+            print("F1 z-score = %f" % f1)
+            print("Acc z-score = %f" % acc)
+#            score, _, pvalue = permutation_test_score(
+#                clf, X_test, y_test, scoring="accuracy", n_permutations=100, n_jobs=1)
+#
+#            print("Classification score %s (pvalue : %s)" % (score, pvalue))
+
+def permutation_distribution(y_true, y_pred, n_permutations=100):
+    """ Compute the distribution of permutations """
+    # Save actual f1_score in front
+    f1 = [f1_score(y_true, y_pred)]
+    acc = [accuracy_score(y_true, y_pred)]
+
+    for i in range(n_permutations):
+        y_sh = np.random.permutation(y_true)
+        f1.append(f1_score(y_sh, y_pred))
+        acc.append(accuracy_score(y_sh, y_pred))
+
+    return zscore(f1, ddof=1)[0], zscore(acc, ddof=1)[0]
 
 
 def convert(data):
