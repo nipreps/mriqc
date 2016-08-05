6c436f691b2c2305f51f83c6febb91d73c11cbb3
oesteban <code@oscaresteban.es>
2016-08-05 11:17:46 -0700

    fix tests

--- build/circle_tests.sh
+++ build/circle_tests.sh
@@ -20,13 +20,13 @@ set -e
 cp build/ants_settings_test.json ~/scratch/data
 
 if [[ $CIRCLE_NODE_INDEX -eq 0 ]]; then
-	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch oesteban/mriqc /scratch/data/ds003_downsampled outputs/ms-func/work -d func -w outputs/ms-func/work
-	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch oesteban/mriqc /scratch/data/ds003_downsampled outputs/ms-anat/work -d anat -w outputs/ms-anat/work --participant_label $(seq -f '%02g' 01 05) --ants-settings /scratch/data/ants_settings_test.json
-	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch oesteban/mriqc /scratch/data/ds003_downsampled outputs/ms-anat/out group -d anat -w outputs/ms-anat/
+	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch oesteban/mriqc /scratch/data/ds003_downsampled outputs/ms-func/out participant -d func -w outputs/ms-func/work
+	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch oesteban/mriqc /scratch/data/ds003_downsampled outputs/ms-anat/out participant -d anat -w outputs/ms-anat/work --participant_label $(seq -f '%02g' 01 05) --ants-settings /scratch/data/ants_settings_test.json
+	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch oesteban/mriqc /scratch/data/ds003_downsampled outputs/ms-anat/out group -d anat -w outputs/ms-anat/work
 fi
 
 if [[ $CIRCLE_NODE_INDEX -eq 1 ]]; then
 	docker run -i -v /etc/localtime:/etc/localtime:ro --entrypoint="/usr/bin/run_tests" oesteban/mriqc
-	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch oesteban/mriqc /scratch/data/ds003_downsampled outputs/ms-anat/work -d anat -w outputs/ms-anat/work --participant_label $(seq -f '%02g' 06 13) --ants-settings /scratch/data/ants_settings_test.json
-	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch oesteban/mriqc /scratch/data/ds003_downsampled outputs/ms-anat/out group -d anat -w outputs/ms-anat/
+	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch oesteban/mriqc /scratch/data/ds003_downsampled outputs/ms-anat/out participant -d anat -w outputs/ms-anat/work --participant_label $(seq -f '%02g' 06 13) --ants-settings /scratch/data/ants_settings_test.json
+	docker run -i -v /etc/localtime:/etc/localtime:ro -v ~/scratch:/scratch -w /scratch oesteban/mriqc /scratch/data/ds003_downsampled outputs/ms-anat/out group -d anat -w outputs/ms-anat/work
 fi
