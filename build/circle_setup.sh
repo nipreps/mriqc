dbc537a332070636e962adc57443ffabf694080e
oesteban <code@oscaresteban.es>
2016-08-05 16:27:34 -0700

    fixing tests

--- build/circle_setup.sh
+++ build/circle_setup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 set -x
-set -e
 
 # Get test data
-if [[ ! -d ${HOME}/testdata/ds003_downsampled ]]; then
+if [[ ! -d ${HOME}/data/ds003_downsampled ]]; then
     # Folder for downloads
     mkdir -p ${HOME}/downloads
     wget -c -P ${HOME}/downloads/ "https://googledrive.com/host/0B2JWN60ZLkgkMEw4bW5VUUpSdFU/ds003_downsampled.tar"
-    rm 'r ${HOME}/testdata/' && mkdir -p ${HOME}/testdata/
-    tar xf ${HOME}/downloads/ds003_downsampled.tar -C ${HOME}/scratch/data
+    tar xf ${HOME}/downloads/ds003_downsampled.tar -C ${HOME}/data
+    rm ${HOME}/downloads/ds003_downsampled.tar
 fi
 
 echo "{plugin: MultiProc, plugin_args: {n_proc: 4}}" > ${HOME}/scratch/plugin.yml
