8a6b100e671b4f22dee6b0399eb8a4bc8bf1a97e
oesteban <code@oscaresteban.es>
2016-09-15 15:37:58 -0700

    update longdesc string

--- mriqc/info.py
+++ mriqc/info.py
@@ -18,14 +18,14 @@ __credits__ = 'Oscar Esteban'
 __license__ = '3-clause BSD'
 __status__ = 'Prototype'
 __description__ = 'NR-IQMs (no-reference Image Quality Metrics) for MRI'
-__longdesc__ = """
-MRIQC provides a series of image processing workflows to extract and compute a series of
-NR (no-reference), IQMs (image quality metrics) to be used in QAPs (quality assessment
+__longdesc__ = """\
+MRIQC provides a series of image processing workflows to extract and compute a series of \
+NR (no-reference), IQMs (image quality metrics) to be used in QAPs (quality assessment \
 protocols) for MRI (magnetic resonance imaging).
-This open-source neuroimaging data processing tool is being developed as a part of the
-MRI image analysis and reproducibility platform offered by the CRN. This pipeline derives
+This open-source neuroimaging data processing tool is being developed as a part of the \
+MRI image analysis and reproducibility platform offered by the CRN. This pipeline derives \
 from, and is heavily influenced by, the PCP Quality Assessment Protocol.
-This tool extracts a series of IQMs from structural and functional MRI data. It is also
+This tool extracts a series of IQMs from structural and functional MRI data. It is also \
 scheduled to add diffusion MRI to the target imaging families.
 """
 
