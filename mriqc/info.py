67854c1ec895e595de8d79f8ee738e6113f607da
oesteban <code@oscaresteban.es>
2016-10-21 08:49:32 -0700

    Finishing up #198
      - Calculates DVARS on the original data (close #221)
      - New carpetplot or motion heatmap (close #152)
      - New parcellation for carpetplot (close #195)
    
    A split-new branch has been opened to keep on working with #216

--- mriqc/info.py
+++ mriqc/info.py
@@ -9,7 +9,7 @@ MRIQC
 import sys
 
 __versionbase__ = '0.8.7'
-__versionrev__ = 'a0'
+__versionrev__ = 'a1'
 __version__ = __versionbase__ + __versionrev__
 __author__ = 'Oscar Esteban'
 __email__ = 'code@oscaresteban.es'
