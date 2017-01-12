6937a576746204fa07d4e5444a86a0e47cb06183
oesteban <code@oscaresteban.es>
2017-01-12 14:04:53 -0800

    Update nipype, niworkflows dependencies in info.py
    Remove link dependencies, since they are not necessary anymore.

--- mriqc/info.py
+++ mriqc/info.py
@@ -7,8 +7,8 @@ MRIQC
 
 """
 
-__versionbase__ = '0.8.9'
-__versionrev__ = '-1'
+__versionbase__ = '0.9.0'
+__versionrev__ = '-rc1'
 __version__ = __versionbase__ + __versionrev__
 __author__ = 'Oscar Esteban'
 __email__ = 'code@oscaresteban.es'
@@ -59,17 +59,13 @@ REQUIRES = [
     'nitime',
     'nilearn',
     'svgutils',
-    'nipype',
+    'nipype>=0.13.0rc1',
     'nipy',
     'statsmodels',
     'pybids'
 ]
 
-LINKS_REQUIRES = [
-    'git+https://github.com/nipy/nipype.git#egg=nipype',
-    'git+https://github.com/poldracklab/niworkflows.git@master#egg=niworkflows',
-    'git+https://github.com/incf/pybids.git@master#egg=pybids',
-]
+LINKS_REQUIRES = []
 
 TESTS_REQUIRES = [
     'mock',
