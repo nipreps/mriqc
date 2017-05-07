a5a25ce770968e8e5acfceebe76b3906af4bf25a
oesteban <code@oscaresteban.es>
2017-05-06 20:23:50 -0700

    [TST] A second try for parallelization
    Based on @chrisfilo's branch

---
+++ .circleci/group.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Balance nipype testing workflows across CircleCI build nodes
+#
+
+# Setting      # $ help set
+set -e         # Exit immediately if a command exits with a non-zero status.
+set -u         # Treat unset variables as an error when substituting.
+set -x         # Print command traces before executing command.
+
+# Exit if build_only tag is found
+if [ "$(grep -qiP 'build[ _]?only' <<< "$GIT_COMMIT_MSG"; echo $? )" == "0" ]; then
+	exit 0
+fi
+
+MODALITY=T1w
+if [ "$CIRCLE_NODE_INDEX" == "1" ]; then
+	MODALITY=bold
+fi
+
+echo "Running group level (${MODALITY} images)..."
+
+docker run -i -v ~/data:/data:ro \
+           -v $SCRATCH:/scratch -w /scratch \
+           ${DOCKER_IMAGE}:${DOCKER_TAG} \
+           /data/${TEST_DATA_NAME} out/ group \
+           -m ${MODALITY}
