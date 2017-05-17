e0fdb0c6ff9478a7207bc633a3d13e57bc648bef
oesteban <code@oscaresteban.es>
2017-05-16 17:19:20 -0700

    update CHANGES and update-changes.sh

--- update-changes.sh
+++ update-changes.sh
@@ -8,6 +8,10 @@ set -e         # Exit immediately if a command exits with a non-zero status.
 set -u         # Treat unset variables as an error when substituting.
 set -x         # Print command traces before executing command.
 
+echo "$1" >> newchanges
+echo $( printf "%${#1}s" | tr " " "=" ) >> newchanges
+echo "" >> newchanges
+
 git log --grep="Merge pull request" `git describe --tags --abbrev=0`..HEAD --pretty='format:  * %b %s' | sed  's/Merge pull request \#\([^\d]*\)\ from\ .*/(\#\1)/' >> newchanges
 echo "" >> newchanges
 echo "" >> newchanges
