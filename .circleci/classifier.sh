d149ad45dc826cb19cd507cbbb9c8dce561b6e45
oesteban <code@oscaresteban.es>
2017-07-17 11:32:54 -0700

    [ENH] Add new classifier to CI testing
    Closes #451

---
+++ .circleci/classifier.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Balance nipype testing workflows across CircleCI build nodes
+#
+
+# Setting      # $ help set
+set -e         # Exit immediately if a command exits with a non-zero status.
+set -u         # Treat unset variables as an error when substituting.
+set -x         # Print command traces before executing command.
+
+# Exit if build_only tag is found
+if [ "$(grep -qiP 'build[ _]?only' <<< "$GIT_COMMIT_MSG"; echo $? )" == "0" ]; then
+    exit 0
+fi
+
+# Exit if docs_only tag is found
+if [ "$(grep -qiP 'docs[ _]?only' <<< "$GIT_COMMIT_MSG"; echo $? )" == "0" ]; then
+    echo "Building [docs_only], nothing to do."
+    exit 0
+fi
+
+if [ "$CIRCLE_NODE_INDEX" == "0" ]; then
+	echo "Testing classifier"
+	docker run -i -v $SCRATCH:/scratch -w /scratch \
+	           --entrypoint="/usr/local/miniconda/bin/mriqc_clf" \
+	           ${DOCKER_IMAGE}:${DOCKER_TAG} \
+	           --load-classifier -X /scratch/out/T1w.csv -o /scratch/out/predicted.csv
+fi
