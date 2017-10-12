6e49a9d8298284f8574ce6b519e26a99b88ede6b
oesteban <code@oscaresteban.es>
2017-10-12 10:39:38 -0700

    update niworkflows dep

--- mriqc/__about__.py
+++ mriqc/__about__.py
@@ -52,7 +52,7 @@ SETUP_REQUIRES = []
 
 REQUIRES = [
     'numpy>=1.12.0',
-    'niworkflows',
+    'niworkflows>=0.1.8',
     'pybids>=0.3.0',
     'scikit-learn>=0.19.0',
     'future',
@@ -75,8 +75,6 @@ REQUIRES = [
 ]
 
 LINKS_REQUIRES = [
-    'git+https://github.com/poldracklab/niworkflows.git'
-    '@2712471d4425b5052622c0b2a40fd6c4c3a3e60b#egg=niworkflows-0.1.8-dev',
 ]
 
 
