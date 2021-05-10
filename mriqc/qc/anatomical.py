886bfc2f42fd7859d267bac599310fa4d517ddfd
RocaVincent <RocaVincent@users.noreply.github.com>
2021-05-10 21:33:33 +0200

    rectifying format

--- mriqc/qc/anatomical.py
+++ mriqc/qc/anatomical.py
@@ -526,7 +526,7 @@ def rpve(pvms, seg):
         upth = np.percentile(pvmap[pvmap > 0], 98)
         loth = np.percentile(pvmap[pvmap > 0], 2)
         pvfs[k] = (
-            pvmap[(pvmap > =0.5) & (pvmap <= upth)].sum() + \
+            pvmap[(pvmap >= 0.5) & (pvmap <= upth)].sum() +
             (1 - pvmap[(pvmap >= loth) & (pvmap < 0.5)]).sum()
         ) / totalvol
     return {k: float(v) for k, v in list(pvfs.items())}
