36badeda58c899df48e6fb3df6a69c87abefacc0
Oscar Esteban <code@oscaresteban.es>
2021-09-20 10:33:58 +0200

    doc: add Apache-2.0 header banner to all files

--- mriqc/bin/messages.py
+++ mriqc/bin/messages.py
@@ -1,3 +1,25 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+#
+# Copyright 2021 The NiPreps Developers <nipreps@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# We support and encourage derived works from this project, please read
+# about our expectations at
+#
+#     https://www.nipreps.org/community/licensing/
+#
 ABIDE_SUBJECT_FETCHED = (
     "Successfully processed subject {subject_id} from site {site_name}"
 )
@@ -18,7 +40,9 @@ CLF_WRONG_PARAMETER_COUNT = "Wrong number of parameters."
 DFCHECK_CSV_CHANGED = "Output CSV file changed one or more values."
 DFCHECK_CSV_COLUMNS = "Output CSV file changed number of columns."
 DFCHECK_DIFFERENT_BITS = "Dataset has different BIDS bits w.r.t. reference."
-DFCHECK_DIFFERENT_LENGTH = "Input datases have different lengths (input={len_input}, reference={len_reference})."
+DFCHECK_DIFFERENT_LENGTH = (
+    "Input datases have different lengths (input={len_input}, reference={len_reference})."
+)
 DFCHECK_IQMS_CORRELATED = "All IQMs show a Pearson correlation >= 0.95."
 DFCHECK_IQMS_UNDER_095 = "IQMs with Pearson correlation < 0.95:\n{iqms}"
 HASH_REPORT = "{sha} {file_name}"
