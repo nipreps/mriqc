46e49385025c71b2bb65561001243166140a26cc
Oscar Esteban <code@oscaresteban.es>
2021-09-20 21:21:46 +0200

    doc: update copyright notice

--- mriqc/__about__.py
+++ mriqc/__about__.py
@@ -26,9 +26,6 @@ from mriqc._version import get_versions
 __version__ = get_versions()["version"]
 del get_versions
 
-__copyright__ = (
-    "Copyright 2020, Center for Reproducible Neuroscience, Stanford University"
-)
-__credits__ = "Oscar Esteban"
+__copyright__ = "Copyright 2020 The NiPreps Developers"
 __download__ = f"https://github.com/poldracklab/mriqc/archive/{__version__}.tar.gz"
-__all__ = ["__version__", "__copyright__", "__credits__", "__download__"]
+__all__ = ["__version__", "__copyright__", "__download__"]
--- mriqc/__init__.py
+++ mriqc/__init__.py
@@ -25,10 +25,9 @@ The mriqc package provides a series of :abbr:`NR (no-reference)`,
 :abbr:`IQMs (image quality metrics)` to used in :abbr:`QAPs (quality
 assessment protocols)` for :abbr:`MRI (magnetic resonance imaging)`.
 """
-from mriqc.__about__ import __copyright__, __credits__, __version__
+from mriqc.__about__ import __copyright__, __version__
 
 __all__ = [
     "__copyright__",
-    "__credits__",
     "__version__",
 ]
