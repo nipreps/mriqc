9ee9f9a06d7f6f257982ca2322b08fd2020e23a1
Oscar Esteban <code@oscaresteban.es>
2023-03-24 17:03:45 +0100

    enh: add code suggestion by @effigies
    Also modify one of the tests including some dwis to check they are indeed filtered out.
    
    Co-authored-by: Chris Markiewicz <effigies@gmail.com>

--- mriqc/config.py
+++ mriqc/config.py
@@ -435,6 +435,11 @@ class execution(_Config):
             ignore_paths = [
                 # Ignore folders at the top if they don't start with /sub-<label>/
                 re.compile(r"^(?!/sub-[a-zA-Z0-9]+)"),
+                # Ignore all modality subfolders, except for func/ or anat/
+                re.compile(
+                    r"^/sub-[a-zA-Z0-9]+(/ses-[a-zA-Z0-9]+)?/"
+                    r"(beh|dwi|fmap|pet|perf|meg|eeg|ieeg|micr|nirs)"
+                ),
                 # Ignore all files, except for the supported modalities
                 re.compile(r"^.+(?<!(_T1w|_T2w|bold))\.(json|nii|nii\.gz)$"),
             ]
