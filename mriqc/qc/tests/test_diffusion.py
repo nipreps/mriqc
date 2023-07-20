dd1309ccfb4be22067dfb962aa29d262fd4539b2
Teresa Gomez <46339554+teresamg@users.noreply.github.com>
2023-07-20 14:46:28 -0400

    Adds initial test functions

--- mriqc/qc/tests/test_diffusion.py
+++ mriqc/qc/tests/test_diffusion.py
@@ -20,12 +20,17 @@
 #
 #     https://www.nipreps.org/community/licensing/
 #
+
 import pytest
 import nibabel as nib
 from dipy.core.gradients import gradient_table
 from dipy.data.fetcher import fetch_sherbrooke_3shell
 import os.path as op
 from ..diffusion import noise_func
+
+import numpy as np
+from mriqc.mriqc.qc import get_spike_mask, get_slice_spike_percentage, get_global_spike_percentage
+
 
 class DiffusionData(object):
     def get_data(self):
@@ -48,3 +53,25 @@ def ddata():
 def test_noise_function(ddata):
     img, gtab = ddata.get_fdata()
     noise_func(img, gtab)
+
+def test_get_spike_mask():
+    spike_mask = get_spike_mask(ddata(), 2)
+
+    assert np.min(np.ravel(spike_mask)) == 0
+    assert np.max(np.ravel(spike_mask)) == 1
+    assert spike_mask.shape == test_data.shape
+
+
+def test_get_slice_spike_percentage():
+    slice_spike_percentage = get_slice_spike_percentage(ddata(), 2, .2)
+
+    assert np.min(np.ravel(slice_spike_percentage)) >= 0
+    assert np.max(np.ravel(slice_spike_percentage)) <= 1
+    assert len(slice_spike_percentage) == ddata().ndim
+
+
+def test_get_global_spike_percentage():
+    global_spike_percentage = get_global_spike_percentage(ddata(), 2)
+
+    assert np.min(np.ravel(global_spike_percentage)) >= 0
+    assert np.max(np.ravel(global_spike_percentage)) <= 1
