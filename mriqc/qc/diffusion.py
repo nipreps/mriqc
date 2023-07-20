d5dd9fce090d206e2e87efa1cd8d95bf735de3d0
Teresa Gomez <46339554+teresamg@users.noreply.github.com>
2023-07-20 14:10:44 -0400

    Adds initial spike functions

--- mriqc/qc/diffusion.py
+++ mriqc/qc/diffusion.py
@@ -21,11 +21,82 @@
 #     https://www.nipreps.org/community/licensing/
 #
 
+import numpy as np
+
+
 """
 Image quality metrics for diffusion MRI data
 ============================================
 """
 
-
 def noise_func(img, gtab):
     pass
+
+def get_spike_mask(data, z_threshold=3):
+    """
+    Return binary mask of spike/no spike
+
+    Parameters
+    ----------
+    data : numpy array
+        Data to be thresholded
+    z_threshold : :obj:`float`
+        Number of standard deviations above the mean to use as spike threshold
+
+    Returns
+    ---------
+    numpy array
+    """
+    threshold = (z_threshold*np.std(np.ravel(data))) + np.mean(np.ravel(data))
+    spike_mask = data > threshold
+
+    return spike_mask
+
+
+def get_slice_spike_percentage(data, z_threshold=3, slice_threshold=.05):
+    """
+    Return percentage of slices spiking along each dimension
+
+    Parameters
+    ----------
+    data : numpy array
+        Data to be thresholded
+    z_threshold : :obj:`float`
+        Number of standard deviations above the mean to use as spike threshold
+    slice_threshold : :obj:`float`
+        Percentage of slice elements that need to be above spike threshold for slice to be considered spiking
+
+    Returns
+    ---------
+    float
+    """
+    spike_mask = get_spike_mask(data, z_threshold)
+
+    ndim = data.ndim
+    slice_spike_percentage = np.zeros(ndim)
+
+    for ii in range(ndim):
+        slice_spike_percentage[ii] = np.mean(np.mean(spike_mask, ii) > slice_threshold)
+
+    return slice_spike_percentage
+
+
+def get_global_spike_percentage(data, z_threshold=3):
+    """
+    Return percentage of array elements spiking
+
+    Parameters
+    ----------
+    data : numpy array
+        Data to be thresholded
+    z_threshold : :obj:`float`
+        Number of standard deviations above the mean to use as spike threshold
+
+    Returns
+    ---------
+    float
+    """
+    spike_mask = get_spike_mask(data, z_threshold)
+    global_spike_percentage = np.mean(np.ravel(spike_mask))
+
+    return global_spike_percentage
