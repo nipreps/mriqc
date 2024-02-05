3a26fbdec224ae7a6d4d4b570e55d1352421aa1e
Teresa Gomez <46339554+teresamg@users.noreply.github.com>
2024-02-05 11:56:22 -0800

    Adds start_on_diffusion functions

--- mriqc/qc/diffusion.py
+++ mriqc/qc/diffusion.py
@@ -1,3 +1,4 @@
+
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 #
@@ -21,18 +22,100 @@
 #     https://www.nipreps.org/community/licensing/
 #
 
-import numpy as np
-
-
 """
 Image quality metrics for diffusion MRI data
 ============================================
 """
 
+import numpy as np
+from dipy.core.gradients import gradient_table
+from dipy.core.gradients import GradientTable
+from dipy.reconst.dti import TensorModel
+from dipy.denoise.noise_estimate import piesno
+from dipy.core.gradients import unique_bvals_magnitude
+from dipy.core.gradients import round_bvals
+
 def noise_func(img, gtab):
     pass
 
-def get_spike_mask(data, grouping_vals, z_threshold=3):
+def noise_b0(data, gtab, mask=None):
+    """
+    Estimate noise in raw dMRI based on b0 variance.
+
+    Parameters
+    ----------
+    """
+    if mask is None:
+        mask = np.ones(data.shape[:3], dtype=bool)
+    b0 = data[..., ~gtab.b0s_mask]
+    return np.percentile(np.var(b0[mask], -1), (25, 50, 75))
+
+
+def noise_piesno(data, n_channels=4):
+    """
+    Estimate noise in raw dMRI data using the PIESNO [1]_ algorithm.
+
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+
+
+    Notes
+    -----
+
+    .. [1] Koay C.G., E. Ozarslan, C. Pierpaoli. Probabilistic Identification
+           and Estimation of Noise (PIESNO): A self-consistent approach and
+           its applications in MRI. JMR, 199(1):94-103, 2009.
+    """
+    sigma, mask = piesno(data, N=n_channels, return_mask=True)
+    return sigma, mask
+
+
+def cc_snr(data, gtab):
+    """
+    Calculate worse-/best-case signal-to-noise ratio in the corpus callosum
+
+    Parameters
+    ----------
+    data : ndarray
+
+    gtab : GradientTable class instance or tuple
+
+    """
+    if isinstance(gtab, GradientTable):
+        pass
+
+    # XXX Per-shell calculation
+    tenmodel = TensorModel(gtab)
+    tensorfit = tenmodel.fit(data, mask=mask)
+
+    from dipy.segment.mask import segment_from_cfa
+    from dipy.segment.mask import bounding_box
+
+    threshold = (0.6, 1, 0, 0.1, 0, 0.1)
+    CC_box = np.zeros_like(data[..., 0])
+
+    mins, maxs = bounding_box(mask)
+    mins = np.array(mins)
+    maxs = np.array(maxs)
+    diff = (maxs - mins) // 4
+    bounds_min = mins + diff
+    bounds_max = maxs - diff
+
+    CC_box[bounds_min[0]:bounds_max[0],
+        bounds_min[1]:bounds_max[1],
+        bounds_min[2]:bounds_max[2]] = 1
+
+    mask_cc_part, cfa = segment_from_cfa(tensorfit, CC_box, threshold,
+                                        return_cfa=True)
+
+    mean_signal = np.mean(data[mask_cc_part], axis=0)
+
+
+def get_spike_mask(data, z_threshold=3, grouping_vals=None, bmag=None):
     """
     Return binary mask of spike/no spike
 
@@ -40,30 +123,42 @@ def get_spike_mask(data, grouping_vals, z_threshold=3):
     ----------
     data : numpy array
         Data to be thresholded
-    grouping_vals : numpy array
-        Values by which to group data for thresholding (bvals or full mask)
     z_threshold : :obj:`float`
         Number of standard deviations above the mean to use as spike threshold
+    grouping_vals : numpy array
+        Values by which to group data for thresholding (bvals or full mask)
+    bmag : int
+        From dipy.core.gradients: 
+        The order of magnitude that the bvalues have to differ to be
+        considered an unique b-value. B-values are also rounded up to
+        this order of magnitude. Default: derive this value from the
+        maximal b-value provided: $bmag=log_{10}(max(bvals)) - 1$.
 
     Returns
     ---------
     numpy array
     """
 
+    if grouping_vals is None:
+        threshold = (z_threshold*np.std(data)) + np.mean(data)
+        spike_mask = data > threshold
+        return spike_mask
+
     threshold_mask = np.zeros(data.shape)
-    gvals = np.unique(grouping_vals) # Not the right function
-    gval_inds = np.where(grouping_vals == gval) # Not the right functions
+
+    rounded_grouping_vals = round_bvals(grouping_vals, bmag)
+    gvals = unique_bvals_magnitude(grouping_vals, bmag)
 
     if grouping_vals.shape == data.shape:
         for gval in gvals:
-            gval_data = data[gval_inds]
+            gval_data = data[rounded_grouping_vals == gval]
             gval_threshold = (z_threshold*np.std(gval_data)) + np.mean(gval_data)
-            threshold_mask[gval_inds] = gval_threshold*np.ones(gval_data.shape)
+            threshold_mask[rounded_grouping_vals == gval] = gval_threshold*np.ones(gval_data.shape)
     else:
         for gval in gvals:
-            gval_data = data[...,gval_inds]
+            gval_data = data[..., rounded_grouping_vals == gval]
             gval_threshold = (z_threshold*np.std(gval_data)) + np.mean(gval_data)
-            threshold_mask[...,gval_inds] = gval_threshold*np.ones(gval_data.shape)
+            threshold_mask[..., rounded_grouping_vals == gval] = gval_threshold*np.ones(gval_data.shape)
 
     spike_mask = data > threshold_mask
 
