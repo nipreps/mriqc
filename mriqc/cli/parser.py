56e68221b9117f55d738ca352cbed079bea8fef4
Zvi Baratz <133042121+zvi-quantivly@users.noreply.github.com>
2024-04-15 14:00:36 +0300

    Fixed string formatting
    Co-authored-by: Oscar Esteban <code@oscaresteban.es>

--- mriqc/cli/parser.py
+++ mriqc/cli/parser.py
@@ -425,7 +425,7 @@ not be what you want in, e.g., shared systems like a HPC cluster.""",
         '--deoblique',
         action='store_true',
         default=False,
-        help='Deoblique the functional scans during head motion correction ' 'preprocessing.',
+        help='Deoblique the functional scans during head motion correction preprocessing.',
     )
     g_func.add_argument(
         '--despike',
